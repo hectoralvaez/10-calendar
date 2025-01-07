54049c1bb0d1e1a04fdab054950d290a31a0fd56
Héctor <hectoralvaez@gmail.com>
2025-01-07 23:39:30 +0100

    ❗❗📅 🌐 414. Axios - Configurar cliente para peticiones HTTP. FIX "baseURL"

--- src/api/calendarApi.js
+++ src/api/calendarApi.js
@@ -4,7 +4,7 @@ import { getEnvVariables } from '../helpers';
 const { VITE_API_URL } = getEnvVariables()
 
 const calendarApi = axios.create({
-    baseURL: 'VITE_API_URL'
+    baseURL: VITE_API_URL
 })
 
 // TODO: Configurar interceptores
